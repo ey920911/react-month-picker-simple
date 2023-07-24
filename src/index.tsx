32415eac4b94cfdf8f9e76b4beea898e67e11f5d
eykim <ncsoft@eykim2-mac.local>
2023-07-25 02:22:58 +0900

    feat: Modify index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,13 @@ import App from './App'
 // import MonthPicker from 'react-month-picker-simple';
 import MonthPicker from './lib/MonthPicker'
 import BasicExample from './playground/basicExample'
-
-const baseStyleEx: CSSProperties = {
-    backgroundColor: 'blue',
-    border: '1px solid white',
-    color: 'white',
-}
-
-const selectedStyleEx: CSSProperties = {
-    backgroundColor: 'white',
-    color: 'black',
-}
+import CustomizedExample from './playground/CustomizedExample'
 
 // Playground
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+    <>
+        <BasicExample />
+        <CustomizedExample />
+    </>,
+)
