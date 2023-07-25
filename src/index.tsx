2bdb6a7b5ace71f542066f3030eeb4913843efe0
eykim <ncsoft@eykim2-mac.local>
2023-07-25 21:21:37 +0900

    feat: Rollback

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
 import React, { CSSProperties } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import BasicExample from './playground/basicExample'
-// import CustomizedExample from './playground/CustomizedExample'
 
-// Playground
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <>
-        {/* <BasicExample />
-        <CustomizedExample />
-        <div>test</div> */}
-        <App />
-    </>,
-)
+root.render(<App />)
