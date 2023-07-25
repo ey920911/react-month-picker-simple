23f79978cf614ab5e7f2786dda02afc6bca99e1a
eykim <ncsoft@eykim2-mac.local>
2023-07-25 21:18:28 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React, { CSSProperties } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import BasicExample from './playground/basicExample'
-import CustomizedExample from './playground/CustomizedExample'
+// import BasicExample from './playground/basicExample'
+// import CustomizedExample from './playground/CustomizedExample'
 
 // Playground
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <>
         {/* <BasicExample />
-        <CustomizedExample /> */}
-        <div>test</div>
+        <CustomizedExample />
+        <div>test</div> */}
+        <App />
     </>,
 )
