e14836a7629b0aeb7842bccd3488c9fa87ac0075
eykim <ncsoft@eykim2-mac.local>
2023-07-25 20:59:44 +0900

    feat: Modify main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React, { CSSProperties } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import MonthPicker from 'react-month-picker-simple';
-import MonthPicker from './lib/MonthPicker'
 import BasicExample from './playground/basicExample'
 import CustomizedExample from './playground/CustomizedExample'
 
 // Playground
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+    <>
+        <BasicExample />
+        <CustomizedExample />
+    </>,
+)
